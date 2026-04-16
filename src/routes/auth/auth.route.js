baa100780995f42a3dab6251aa8c97e67307ad35
Cristhian Rodríguez <crisrodam1996@gmail.com>
2026-04-15 19:31:33 -0500

    Ruta para cerrar sesión

--- src/routes/auth/auth.route.js
+++ src/routes/auth/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { authControllers } from '../../controllers/index.controllers.js'
+import { authMiddleware } from '../../middlewares/index.middlewares.js'
 
 const authRouter = Router()
 
 authRouter.post('/iniciar-sesion', authControllers.login)
-authRouter.patch('/cerrar-sesion', authControllers.logout)
+authRouter.patch('/cerrar-sesion', authMiddleware.handleAuth, authControllers.logout)
 
 export default authRouter
